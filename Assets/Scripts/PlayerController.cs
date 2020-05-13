2e3bb0edda00e1de114ba55706fc121f086e9034
JamiReinikainen <gr189348@gradia.fi>
2020-05-13 14:23:18 +0300

    mentalisti

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 10.0f;
+    // jos luku on sama esim 15 ja - 15 voit tehdä yhden variablen
+    public float Xrange = 15.0f;
+
+    public GameObject projectilePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < -Xrange)
+        {
+            transform.position = new Vector3(-Xrange, transform.position.y, transform.position.z);
+        }
+        
+        if (transform.position.x > Xrange)
+        {
+            transform.position = new Vector3(Xrange, transform.position.y, transform.position.z);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Heittää ruuan
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+    }
+}
